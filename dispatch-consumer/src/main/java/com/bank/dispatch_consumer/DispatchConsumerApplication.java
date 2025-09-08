464b1f8495bed1d387521beb146715d227dba08f
verdugox <luisandresverdugox@gmail.com>
2025-09-08 10:33:57 -0500

    feat: Subiendo todas las configuraciones y dependencias necesarias para empezar el proyecto desde 0

---
+++ dispatch-consumer/src/main/java/com/bank/dispatch_consumer/DispatchConsumerApplication.java
@@ -0,0 +1,13 @@
+package com.bank.dispatch_consumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DispatchConsumerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DispatchConsumerApplication.class, args);
+	}
+
+}
---
+++ dispatch-consumer/src/test/java/com/bank/dispatch_consumer/DispatchConsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bank.dispatch_consumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DispatchConsumerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
